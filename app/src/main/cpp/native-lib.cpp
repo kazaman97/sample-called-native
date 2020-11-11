21ea6aabe63a1cd44e13fb7dd7b6cd3ef6d70400
kazaman97 <narusai97@gmail.com>
2020-11-12 00:54:12 +0900

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,66 @@
+#include <jni.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <filesystem>
+
+#include "native-lib.h"
+#include "android/log.h"
+
+using namespace std;
+
+#define TEST "./test.txt"
+
+#define LOGE(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+
+bool readFile(const std::string& path) {
+    ifstream ifs(path);
+
+    char str[BUFF_SIZE];
+    if (ifs.fail()) {
+        LOGE("failed read file");
+        return false;
+    }
+    while (ifs.getline(str, BUFF_SIZE)) {
+        LOGE("%s", str);
+    }
+    return true;
+}
+
+//bool printCurrentFileName() {
+////    filesystem::directory_iterator iterator;
+////    error_code err;
+//
+//    for (const auto & entry : filesystem::directory_iterator("./")) {
+//
+////        file_names.push_back( entry.path().string() );
+////        printf("%s\n", file_names.back().c_str());
+////        LOGE("printCurrentFileName: %s", entry.path().c_str());
+//    }
+//
+//    /* エラー処理 */
+////    if (err) {
+////        std::cout << err.value() << std::endl;
+////        std::cout << err.message() << std::endl;
+////        return false;
+////    }
+//    return true;
+//}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_sample_1called_1native_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    bool isRead = readFile(TEST);
+//    bool isPrintFileName = printCurrentFileName();
+
+    stringstream stringStream;
+    stringStream << "readFile: ";
+    stringStream << (isRead ? "Success" : "Failed") << endl;
+//    stringStream << "printCurrentFileName";
+//    stringStream << (isPrintFileName ? "Success" : "Failed") << endl;
+
+    return env->NewStringUTF(stringStream.str().c_str());
+//    return env->NewStringUTF("hello");
+}
---
+++ app/src/main/cpp/native-lib.h
@@ -0,0 +1,13 @@
+//
+// Created by KazamaRyusei on 2020/11/12.
+//
+
+#ifndef SAMPLE_CALLED_NATIVE_NATIVE_LIB_H
+#define SAMPLE_CALLED_NATIVE_NATIVE_LIB_H
+
+#define TAG "NATIVE_LIB"
+#define BUFF_SIZE 100
+
+bool readFile(const std::string& path);
+
+#endif //SAMPLE_CALLED_NATIVE_NATIVE_LIB_H
