8860c4520c49e145d0e286fe51e061f684b55ad1
kazaman97 <narusai97@gmail.com>
2020-11-13 00:08:27 +0900

    modify code

---
+++ app/src/main/java/com/example/sample_called_native/CallFromNative.kt
@@ -0,0 +1,6 @@
+package com.example.sample_called_native
+
+class CallFromNative {
+    companion object {
+    }
+}
--- app/src/main/java/com/example/sample_called_native/MainActivity.kt
+++ app/src/main/java/com/example/sample_called_native/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val sampleHashMap = HashMap<String, String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sampleHashMap["okinawa"] = "naha"
+        sampleHashMap["iwate"] = "morioka"
 
         // Example of a call to a native method
         findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
